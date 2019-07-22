873263a5768d98132fef7391cbb21b1945c2b302
Bryan Szendel <bryan.szendel@gmail.com>
2019-07-22 14:42:40 -0600

    movie cards are rendering on screen

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,9 @@
 import React, { useState } from 'react';
 import { Route } from 'react-router-dom';
 
-import { SavedList } from './Movies/SavedList';
-import { MovieList } from './Movies/MovieList';
-import { Movie } from './Movies/Movie.js';
+import SavedList from './Movies/SavedList';
+import MovieList from './Movies/MovieList';
+import Movie from './Movies/Movie';
 
 const App = () => {
   const [savedList, setSavedList] = useState( [] );
