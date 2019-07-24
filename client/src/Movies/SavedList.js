2e0ef1c08d108829350c17ae3ff8b73005358dbc
Bryan Szendel <bryan.szendel@gmail.com>
2019-07-23 18:36:34 -0600

    STRETCH COMPLETE - saved movies are linking back to movie card

--- client/src/Movies/SavedList.js
+++ client/src/Movies/SavedList.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
+import { Link, NavLink } from 'react-router-dom';
 
 const SavedList = props => (
   <div className="saved-list">
@@ -7,7 +7,9 @@ const SavedList = props => (
     <h3>Saved Movies:</h3>
     {props.list.map(movie => (
       <ul>
-        <li key={movie.title} className="saved-movie">{movie.title}</li>
+        <li>
+          <NavLink to={`/movies/${movie.id}`} key={movie.title} className="saved-movie">{movie.title}</NavLink>
+        </li>
       </ul>
     ))}
     <Link to="/" className="home-button">Home</Link>
